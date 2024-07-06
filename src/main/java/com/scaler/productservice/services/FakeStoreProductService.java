db939ac7148c6055b2cb17a19f866bd1f6b215cc
krishnaaag <krishnaagrawal1990@gmail.com>
2024-07-06 10:25:34 +0530

    Update API

--- src/main/java/com/scaler/productservice/services/FakeStoreProductService.java
+++ src/main/java/com/scaler/productservice/services/FakeStoreProductService.java
@@ -3,13 +3,18 @@ package com.scaler.productservice.services;
 import com.scaler.productservice.dtos.FakeStoreProductDto;
 import com.scaler.productservice.models.Category;
 import com.scaler.productservice.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpMessageConverterExtractor;
+import org.springframework.web.client.RequestCallback;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+
 public class FakeStoreProductService implements ProductService{
 
     private RestTemplate restTemplate;
@@ -42,6 +47,34 @@ public class FakeStoreProductService implements ProductService{
         }
         return products;
     }
+
+    //Partial update
+    //Response type- .class
+    //We got these lines from restTemplate.patchForObject as they have no return type that's why we used lines where return mentioned
+
+    @Override
+    public Product updateProduct(Long id, Product product) {
+        //PATCH - Input object - Product
+        RequestCallback requestCallback = restTemplate.httpEntityCallback(product, FakeStoreProductDto.class);
+        HttpMessageConverterExtractor<FakeStoreProductDto> responseExtractor = new HttpMessageConverterExtractor(FakeStoreProductDto.class, restTemplate.getMessageConverters());
+        FakeStoreProductDto response= restTemplate.execute("https://fakestoreapi.com/products/"+ id, HttpMethod.PATCH, requestCallback, responseExtractor);
+
+        //Convert FakeStoreDTO to product
+        return convertFakeStoreProductToProduct(response);
+    }
+
+    @Override
+    public Product replaceProduct(Long id, Product product) {
+        //PUT
+
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+
+    }
+
     private Product convertFakeStoreProductToProduct(FakeStoreProductDto fakeStoreProductDto) {
         Product product = new Product();
         product.setId(fakeStoreProductDto.getId());
@@ -49,7 +82,7 @@ public class FakeStoreProductService implements ProductService{
         product.setPrice(fakeStoreProductDto.getPrice());
 
         Category category = new Category();
-        category.setDescription(fakeStoreProductDto.getCategory());
+        category.setDescription(fakeStoreProductDto.getDescription());
         product.setCategory(category);
 
         return product;
--- src/main/java/com/scaler/productservice/services/ProductService.java
+++ src/main/java/com/scaler/productservice/services/ProductService.java
@@ -11,6 +11,12 @@ public interface ProductService {
 
     List<Product> getAllProducts();
 
+    Product updateProduct(Long id, Product product);
+
+    Product replaceProduct(Long id, Product product);
+
+    void  deleteProduct(Long id);
+
 
 
 }
