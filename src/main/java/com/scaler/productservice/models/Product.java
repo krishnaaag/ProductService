ef620cd3532af22be0fda622417ef16d5babf590
krishnaaag <krishnaagrawal1990@gmail.com>
2024-07-15 23:51:31 +0530

    Database connection

--- src/main/java/com/scaler/productservice/models/Product.java
+++ src/main/java/com/scaler/productservice/models/Product.java
@@ -1,14 +1,32 @@
 package com.scaler.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel {
     private String title;
     private double price;
+    @ManyToOne
     private Category category;
-
   }
+
+  /*
+
+  Product ----------- Category => M:1
+     M                   1
+
+    */
+
+
+
+
+
 
